a93f669025e8b44772cd38618401bed312289b14
simonysh029 <simonysh029@naver.com>
2023-05-02 18:04:06 +0900

    lesson02

---
+++ chap02/src/sec02/exam08/BooleanExample.java
@@ -0,0 +1,14 @@
+package sec02.exam08;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean stop = true;
+		if(stop) {
+			System.out.println("중지합니다.");
+		} else {
+			System.out.println("시작합니다.");
+		}
+	}
+}
+// primitive 자바에서 만들어준 원시어? 원시변수? : long short byte double char float boolean int
---
+++ chap02/src/sec03/exam02/CastingExample.java
@@ -0,0 +1,21 @@
+package sec03.exam02;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		int intValue = 44032;
+		char charValue = (char) intValue;
+		System.out.println(charValue);
+		
+		long longValue = 500;
+		intValue = (int) longValue;
+		System.out.println(intValue);
+		
+		double doubleValue = 3.14;
+		intValue = (int) doubleValue;
+		System.out.println(intValue);
+	}
+
+}
+
+// 강제 형변환 casting <-> 자동 형변환 promotion
