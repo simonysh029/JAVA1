5226982dadeda139a40350f1dfcb8daba40e2b79
simonysh029 <simonysh029@naver.com>
2023-05-25 18:19:29 +0900

    0525working

---
+++ chap06/src/sec05/exam04/Singleton.java
@@ -0,0 +1,15 @@
+package sec05.exam04;
+
+public class Singleton {
+	// 정적 필드(객체 생성을 통해서 생성한...)
+	private static Singleton singleton = new Singleton(); //객체 생성
+	
+	// 생성자
+	private Singleton() {}
+	
+	// 정적 메소드
+	static Singleton getInstance() {
+		return singleton;
+	}
+
+}
