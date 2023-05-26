4733b209bad1b0f3c16cec8ae54dfc849e2c854c
simonysh029 <simonysh029@naver.com>
2023-05-26 18:25:07 +0900

    master0526

---
+++ chap06/src/sec06/exam02/mycompany/Car.java
@@ -0,0 +1,17 @@
+package sec06.exam02.mycompany;
+
+import sec06.exam02.hankook.*; // Tire
+import sec06.exam02.kumho.*; // BigWidthTire, Tire
+import sec06.exam02.hyundai.Engine; // Engine
+
+public class Car {
+	//필드
+	Engine engine = new Engine();
+	SnowTire tire1 = new SnowTire();
+	BigWidthTire tire2 = new BigWidthTire();
+	
+	sec06.exam02.hankook.Tire tire3 = new sec06.exam02.hankook.Tire();
+	sec06.exam02.kumho.Tire tire4 = new sec06.exam02.kumho.Tire();
+	
+}
+//ctrl shift o
